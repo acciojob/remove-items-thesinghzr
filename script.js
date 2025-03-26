e7299f48f7b274f21e6e4f46e6346f3fe5e1c4e1
Suraj Singh <yousurajsingh@gmail.com>
2025-03-27 01:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function removeColor() {
+    let select = document.getElementById("colorSelect");
+
+    if (select.options.length > 0 && select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+    }
+}
